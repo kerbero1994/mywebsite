c34996777acf48027b02f7c69b7f0efeebba083b
Eduglez94 <eduglezzvl_ing@outlook.es>
2017-02-24 03:58:15 -0600

    add flat color and js for animation
    change color

--- Personal_page_h1/stylesheet/javascript.js
+++ Personal_page_h1/stylesheet/javascript.js
@@ -1,5 +1,28 @@
-function moveTo(blog) {
-	element= document.getElementById(blog);
-	console.log(element);
-	element.scrollIntoView(true);
-}
+$(function() {
+
+    $('a[href*=#]').click(function() {
+
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname) {
+
+            var $target = $(this.hash);
+
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+
+            if ($target.length) {
+
+                var targetOffset = $target.offset().top;
+
+                $('html,body').animate({
+                    scrollTop: targetOffset
+                }, 500);
+
+                return false;
+
+            }
+
+        }
+
+    });
+
+});
